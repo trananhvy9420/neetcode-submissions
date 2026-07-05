3cff10f3a4649992cab7713e49ef08841e4e3634
Trần Anh Vỹ <trananhvy9420@gmail.com>
2026-07-05 21:34:12 +0700

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let response = "";
+        for (const str of strs){
+            response += `${str.length}#${str}`;
+        }
+        return response;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    //6#Design2#An9#Algorithm'
+    //012345678
+    //["","vn"]
+    //0#2vn
+    //1#V4#Grz/
+    //2#we3#say1#:3#yes10#!@#$%^&*()
+    decode(str) {
+        let array = [];
+        let i = 0;
+        while (i < str.length) {
+            let j = i;
+            while (str[j] !== '#') {
+                j++;
+            }
+            let length = parseInt(str.substring(i, j));
+            i = j + 1;
+            array.push(str.substring(i, i + length));
+            i += length;
+        }
+        return array;
+    }
+}
