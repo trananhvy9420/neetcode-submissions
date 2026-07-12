22c388ae731c67df364069b3dd504c5d9fa8cc4f
Trần Anh Vỹ <trananhvy9420@gmail.com>
2026-07-12 16:06:09 +0700

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        const res = new Set();
+        nums.sort((a,b) => a-b );
+        for ( let i = 0 ; i < nums.length ; i++ ){
+            for ( let j = i + 1 ; j < nums.length ; j++ ){
+                for ( let k = j + 1 ; k < nums.length ; k++ ){
+                    if(nums[i] + nums[j] + nums[k] === 0 ) {
+                        res.add(JSON.stringify([nums[i], nums[j], nums[k]]));
+                    } 
+                }
+            }
+        }
+        return Array.from(res).map((item) => JSON.parse(item));
+    }
+}
